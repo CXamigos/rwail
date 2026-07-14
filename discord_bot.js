34231b93b398e6797a6f842a4587dd7337b3dfc9
CXamigos <glyconbyfron@gmail.com>
2026-07-14 20:59:08 +0300

    Update discord_bot.js

--- discord_bot.js
+++ discord_bot.js
@@ -3518,8 +3518,8 @@ client.on("interactionCreate", async (interaction) => {
             : "Auto4";
         const amount =
             isFarm && isBypassUser
-                ? interaction.options.getInteger("amount") || 15
-                : 15;
+                ? interaction.options.getInteger("amount") || 17
+                : 17;
 
         const initialHash = hashInput.startsWith("#")
             ? hashInput
